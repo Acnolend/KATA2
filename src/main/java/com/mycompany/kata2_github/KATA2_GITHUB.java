c0fc24f2edb9acd4b5ed36e7c848d67ad7bb5b62
ADMIN <ADMIN@DESKTOP-UFH4UD2>
2022-10-05 09:53:45 +0100

    version 3

--- src/main/java/com/mycompany/kata2_github/KATA2_GITHUB.java
+++ src/main/java/com/mycompany/kata2_github/KATA2_GITHUB.java
@@ -1,18 +1,13 @@
 package com.mycompany.kata2_github;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class KATA2_GITHUB {
 
     public static void main(String[] args) {
         int[] data = {4, 1, 2, 4, 7, 9, 3, 5, 7, 2, 0, 1, 3, 5, 3, 3, 5};
-        Map<Integer, Integer> histogram = new HashMap<>();
-        
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? 
-                            histogram.get(key) + 1 : 1);
-        }  
+        Histogram generator = new Histogram(data);
+        Map<Integer, Integer> histogram = generator.getHistogram();
             
         for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + " ===> " + entry.getValue());
